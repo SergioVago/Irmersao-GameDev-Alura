15d5b343b0d14c149d6d3482effe804077cef54e
Sergio Vago <sergio.vago@hotmail.com>
2020-06-24 19:53:10 -0300

    Ajuste na responsividade dos tamanos dos personagens

--- sketch.js
+++ sketch.js
@@ -12,6 +12,10 @@ let inimigo
 const matrizPersonagem = calculaMatriz(880, 1080, 4, 4)
 const matrizInimigo = calculaMatriz(420, 700, 4, 7)
 
+function calculaProporcao() {
+  return width > height ? width * 0.0004 : height * 0.001
+}
+
 function preload() {
   imagemCenario = loadImage('imagens/cenario/floresta.png')
   imagemPersonagem = loadImage('imagens/personagem/correndo.png')
@@ -22,15 +26,15 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario, 3)
-  personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 110, 135, 220, 270)
-  inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 52, 52, 104, 104)
+  personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 220 * calculaProporcao(), 270 * calculaProporcao(), 220, 270)
+  inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 104 * calculaProporcao(), 104 * calculaProporcao(), 104, 104)
 
   frameRate(40)
   // musica.loop()
 }
 
 function keyPressed() {
-  if(key === 'ArrowUp') {
+  if (key === 'ArrowUp') {
     personagem.pula(70)
   }
 }
@@ -45,7 +49,7 @@ function draw() {
   inimigo.exibe()
   inimigo.moveX(10)
 
-  if(personagem.estaColidindo(inimigo)) {
+  if (personagem.estaColidindo(inimigo)) {
     console.log('Colidu');
     noLoop()
   }
